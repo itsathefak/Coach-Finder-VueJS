af8c3158d22493b96daacf949a24979c0df98419
Athef Ak <athefak@92:06:cb:57:d6:bf.home>
2025-02-05 19:11:20 -0500

    Wiring routes to the components

--- src/router.js
+++ src/router.js
@@ -1,17 +1,30 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
+import CoachDetails from './pages/coaches/CoachDetails.vue'
+import CoachList from './pages/coaches/CoachesList.vue'
+import CoachRegistration from './pages/coaches/CoachesRegister.vue'
+import ContactCoach from './pages/requests/ContactCoach.vue'
+import RequestsReceived from './pages/requests/RequestReceived.vue'
+import NotFound from './pages/NotFound.vue'
+
+
+
 const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-        { path: '/', redirect: '/coaches' },
-        { path: 'coaches', component: null },
-        { path: '/coaches/:id', component: null, children:[
-            { path: 'contact', component: null},  //coaches/c1/contact
-        ]},
-        { path: 'register', component: null},
-        { path: '/request', component: null},
-        { path: '/:notFound(.*)', component: null}
-    ]
-})
+  history: createWebHistory(),
+  routes: [
+    { path: '/', redirect: '/coaches' },
+    { path: 'coaches', component: CoachList },
+    {
+      path: '/coaches/:id',
+      component: CoachDetails,
+      children: [
+        { path: 'contact', component: ContactCoach }, //coaches/c1/contact
+      ],
+    },
+    { path: 'register', component: CoachRegistration },
+    { path: '/request', component: RequestsReceived },
+    { path: '/:notFound(.*)', component: NotFound },
+  ],
+});
 
 export default router;
