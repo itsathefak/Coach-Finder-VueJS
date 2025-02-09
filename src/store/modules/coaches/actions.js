bf5461acbd55d01df38ecbc408815221b15f275e
Athef Ak <athefak@Athefs-MacBook-Pro.local>
2025-02-09 12:47:03 -0500

    Fetching Coach Data using GET

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -27,4 +27,27 @@ export default {
       id: userId
     });
   },
+
+  async loadCoaches (context){
+   const response = await fetch(`https://coach-finder-76372-default-rtdb.firebaseio.com/coaches.json`);
+    const responseData = await response.json();
+
+    if(!response.ok){
+      // ...
+    }
+    const coaches = []
+
+    for (const key in responseData ){
+      const coach = {
+        firstName: responseData[key].firstName,
+        lastName: responseData[key].lastName,
+        description: responseData[key].description,
+        hourlyRate: responseData[key].hourlyRate,
+        areas: responseData[key].areas,
+      }
+      coaches.push(coach);
+    }
+
+    context.commit('setCoaches', coaches)
+  }
 };
