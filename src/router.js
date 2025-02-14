f5a3855f8a1d368a290ff47236e1f6af19a8b1ce
Athef Ak <athefak@16:56:8b:be:4f:7e.home>
2025-02-13 21:39:22 -0500

    Use Async components for optimization

--- src/router.js
+++ src/router.js
@@ -1,15 +1,20 @@
+import { defineAsyncComponent } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
-import CoachDetails from './pages/coaches/CoachDetails.vue'
+// import CoachDetails from './pages/coaches/CoachDetails.vue'
 import CoachList from './pages/coaches/CoachesList.vue'
-import CoachRegistration from './pages/coaches/CoachRegistration.vue'
-import ContactCoach from './pages/requests/ContactCoach.vue'
-import RequestsReceived from './pages/requests/RequestReceived.vue'
+// import CoachRegistration from './pages/coaches/CoachRegistration.vue'
+// import ContactCoach from './pages/requests/ContactCoach.vue'
+// import RequestsReceived from './pages/requests/RequestReceived.vue'
 import NotFound from './pages/NotFound.vue'
-import UserAuth from './pages/auth/UserAuth.vue';
+// import UserAuth from './pages/auth/UserAuth.vue';
 import store from './store/index'
 
-
+const CoachDetails = defineAsyncComponent(()=> import('./pages/coaches/CoachDetails.vue'))
+const CoachRegistration = defineAsyncComponent(()=> import('./pages/coaches/CoachRegistration.vue'))
+const ContactCoach = defineAsyncComponent(()=> import('./pages/requests/ContactCoach.vue'))
+const RequestsReceived = defineAsyncComponent(()=> import('./pages/requests/RequestReceived.vue'))
+const UserAuth = defineAsyncComponent(()=> import('./pages/auth/UserAuth.vue'))
 
 const router = createRouter({
   history: createWebHistory(),
